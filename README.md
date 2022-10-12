b0f87e788bde0d0d61cd391caf3b2b31235e8a22
Oliver Schiøtt <oliver.schiott@outlook.com>
2022-10-12 17:32:56 +0200

    Work

--- README.md
+++ README.md
@@ -2,3 +2,42 @@
 
 Henter værmelding fra  open-meteo.com
 https://open-meteo.com/en/docs
+
+## Yr
+
+https://hjelp.yr.no/hc/no/articles/203786121-V%C3%A6rsymbolene-p%C3%A5-Yr-
+
+## Youtube
+
+https://www.youtube.com/watch?v=GVrjv9ajsJ0
+
+
+## Oppgaveteksten
+Du skal i denne oppgaven lage en nettside som viser værmeldingen for Oslo de neste 7 dagene. 
+Nettsiden skal bestå av 3 html-sider. 
+
+En som viser været akkurat nå på en god måte, 
+1 som viser en tabell over vært de neste 7 dagene 
+og en som viser et plot over forventet temperatur de neste 7 dagene. 
+Nettsiden skal inneholde en menylinje for å navigere mellom de ulike sidene. 
+ 
+For å få værmeldingen skal dere bruke nettsiden open-meteo.com. Dere trenger ikke å lage en bruker. 
+ 
+Du skal lage en 5 min lang video hvor dere viser nettsiden, og forklarer koden.
+ 
+Skal innleveres:
+1. Kode i .zip-fil på it´s learning
+2. Video på 5 min
+	 
+Suksesskriterier:
+· Har laget en god menylinje
+· Har laget 3 fine nettsider. Tenk på layout av sidene. 
+· Har skrevet oversiktlig HTML
+· Har skrevet oversiktlig CSS
+· Har skrevet oversiktlig Javascript
+· Har laget riktig API-kall
+· Har laget ett godt plot av temperatur med plotly
+· Har oversiktlig mappestruktur og navn på filer
+· Levert i riktig format til riktig tid
+· Laget en god video som forklarer koden på en oversiktlig måte
+· Har med noe unikt og kreativt på nettsiden sin
--- WeatherForecast/Models/WeatherCodes.cs
+++ WeatherForecast/Models/WeatherCodes.cs
@@ -5,51 +5,59 @@ namespace WeatherForecast.Models
     public static class WeatherCodes
     {
 
+        public static string GetWeatherDescription(int weathercode)
+        {
+            foreach (var item in Weathercodes)
+            {
+                if (item.Key == weathercode)
+                {
+                    return item.Value;
+                }
+            }
 
-        public static List<KeyValuePair<int, string>> Weathercodes = new List<KeyValuePair<int, string>>
+            return $"Sorry, vi fant ikke beskrivelsen tilknyttet værkoden {weathercode}";
+
+        }
+
+
+        private static List<KeyValuePair<int, string>> Weathercodes = new List<KeyValuePair<int, string>>
         {
 
-        new KeyValuePair<int, string>(0,    "Clear sky"),
-        new KeyValuePair<int, string>(1,"Mainly clear, partly cloudy, and overcast"),
-        new KeyValuePair<int, string>(2,"Mainly clear, partly cloudy, and overcast"),
-            new KeyValuePair<int, string>(3,    "Mainly clear, partly cloudy, and overcast"),
-        new KeyValuePair<int, string>(45,  "Fog and depositing rime fog"),
-        new KeyValuePair<int, string>(48,    "Fog and depositing rime fog"),
+            new KeyValuePair<int, string>(0, "Clear sky"),
+            new KeyValuePair<int, string>(1, "Mainly clear"),
+            new KeyValuePair<int, string>(2, "Partly cloudy"),
+            new KeyValuePair<int, string>(3, "Overcast"),
 
-        new KeyValuePair<int, string>(51,   "Drizzle: Light intensity"),
-        new KeyValuePair<int, string>(53,   "Drizzle: moderate intensity"),
-        new KeyValuePair<int, string>(55 ,   "Drizzle: dense intensity"),
+            new KeyValuePair<int, string>(45, "Fog"),
+            new KeyValuePair<int, string>(48, "Depositing rime fog"),
 
-        new KeyValuePair<int, string>(56,     "Freezing Drizzle: Light and dense intensity"),
-        new KeyValuePair<int, string>(57  ,  "Freezing Drizzle: Light and dense intensity"),
+            new KeyValuePair<int, string>(51, "Drizzle: Light intensity"),
+            new KeyValuePair<int, string>(53, "Drizzle: moderate intensity"),
+            new KeyValuePair<int, string>(55, "Drizzle: dense intensity"),
 
+            new KeyValuePair<int, string>(56, "Freezing Drizzle: Light intensity"),
+            new KeyValuePair<int, string>(57, "Freezing Drizzle: Dense intensity"),
 
-        new KeyValuePair<int, string>(61, "Rain: Slight, moderate and heavy intensity"),
-        new KeyValuePair<int, string>(63,"Rain: Slight, moderate and heavy intensity"),
-        new KeyValuePair<int, string>(65,    "Rain: Slight, moderate and heavy intensity"),
+            new KeyValuePair<int, string>(61, "Rain: Slight intensity"),
+            new KeyValuePair<int, string>(63, "Rain: Moderate intensity"),
+            new KeyValuePair<int, string>(65, "Rain: Heavy intensity"),
 
-        new KeyValuePair<int, string>(66, " Freezing Rain: Light and heavy intensity"),
-        new KeyValuePair<int, string>(67  , "Freezing Rain: Light and heavy intensity"),
+            new KeyValuePair<int, string>(66, "Freezing Rain: Light intensity"),
+            new KeyValuePair<int, string>(67, "Freezing Rain: Heavy intensity"),
+            new KeyValuePair<int, string>(71, "Snow fall: Slight intensity"),
+            new KeyValuePair<int, string>(73, "Snow fall: Moderate intensity"),
+            new KeyValuePair<int, string>(75, "Snow fall: Heavy intensity"),
+            new KeyValuePair<int, string>(77, "Snow grains"),
 
-
-        //new KeyValuePair<int, string>(71, 73, 75    Snow fall: Slight, moderate, and heavy intensity"),
-        //new KeyValuePair<int, string>(71, 73, 75    Snow fall: Slight, moderate, and heavy intensity"),
-        //new KeyValuePair<int, string>(71, 73, 75    Snow fall: Slight, moderate, and heavy intensity"),
-
-        //new KeyValuePair<int, string>(77    Snow grains
-        //new KeyValuePair<int, string>(80, 81, 82    Rain showers: Slight, moderate, and violent"),
-        //new KeyValuePair<int, string>(80, 81, 82    Rain showers: Slight, moderate, and violent"),
-        //new KeyValuePair<int, string>(80, 81, 82    Rain showers: Slight, moderate, and violent"),
-
-
-        //new KeyValuePair<int, string>(85, 86    Snow showers slight and heavy"),
-        //new KeyValuePair<int, string>(85, 86    Snow showers slight and heavy"),
-
-        //new KeyValuePair<int, string>(95 *  Thunderstorm: Slight or moderate"),
-        //new KeyValuePair<int, string>(96, 99 *  Thunderstorm with slight and heavy hail"),
-        //new KeyValuePair<int, string>(96, 99 *  Thunderstorm with slight and heavy hail"),
-
-            };
+            new KeyValuePair<int, string>(80, "Rain showers: Slight"),
+            new KeyValuePair<int, string>(81, "Rain showers: Moderate"),
+            new KeyValuePair<int, string>(82, "Rain showers: Violent"),
+            new KeyValuePair<int, string>(85, "Snow showers Slight"),
+            new KeyValuePair<int, string>(86, "Snow showers Heavy,"),
+            new KeyValuePair<int, string>(95, "Thunderstorm: Slight or moderate"),
+            new KeyValuePair<int, string>(96, "Thunderstorm with slight and heavy hail"),
+            new KeyValuePair<int, string>(99, "Thunderstorm with slight and heavy hail"),
+        };
     }
 }
 
